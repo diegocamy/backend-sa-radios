b6b70e83ef200e5d34360e668ebbad7c2c622be8
dcamy <diegocamyy@gmail.com>
2021-05-05 14:15:19 -0300

    Refactored the code to use Axios and form-data to identify a song. Also added comments and fixed the error handlers

--- server.ts
+++ server.ts
@@ -3,11 +3,13 @@ import cors from "cors";
 import helmet from "helmet";
 import multer from "multer";
 import fs from "fs";
-import request, { Options } from "request";
 import dotenv from "dotenv";
+import axios, { AxiosRequestConfig } from "axios";
+const FormData = require("form-data");
 const SoundCloud = require("soundcloud-scraper");
 dotenv.config();
 
+//INTERFACES
 interface ResponseError extends Error {
   status?: number;
 }
@@ -16,9 +18,7 @@ const app = express();
 
 //MULTER CONFIG
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./tmp");
-  },
+  destination: "./tmp",
   filename: (req, file, cb) => {
     cb(null, "blob-" + Date.now());
   },
@@ -27,60 +27,86 @@ const upload = multer({ storage });
 
 //MIDDLEWARES
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: process.env.ORIGIN || "http://localhost:3001" }));
 app.use(helmet());
 
 //ROUTES
 app.post("/soundcloud", async (req, res, next) => {
   try {
+    //extract the radio url from request body
     const { radioURL } = req.body;
+    //create soundcloud client
     const client = new SoundCloud.Client();
+    //fetch the streamurl from the radio url
     const { streamURL } = await client.getSongInfo(radioURL, {
       fetchStreamURL: true,
     });
-
+    //return the stream url
     res.send(streamURL);
   } catch (error) {
     next(error);
   }
 });
 
-app.post("/shazam", upload.single("file"), (req, res, next) => {
-  try {
-    //get the file from the request object
-    const recording = req.file;
+app.post("/shazam", upload.single("file"), async (req, res, next) => {
+  //create array from api keys
+  const api_keys = process.env.RAPIDAPI_KEY?.split(",");
+  let keyNumber = 0;
 
-    //request options
-    const options: Options = {
-      method: "POST",
-      url: "https://shazam-core.p.rapidapi.com/v1/tracks/recognize",
-      headers: {
-        "content-type":
-          "multipart/form-data; boundary=---011000010111000001101001",
-        "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-        "x-rapidapi-host": "shazam-core.p.rapidapi.com",
-        useQueryString: true,
-      },
-      formData: {
-        file: {
-          value: fs.createReadStream(recording.path),
-          options: {
-            __filename: recording.filename,
-            contentType: "audio/wav",
-          },
-        },
-      },
+  //get the file from the request object
+  const recording = req.file;
+
+  //create form-data
+  const form = new FormData();
+  form.append("file", fs.createReadStream(recording.path), {
+    filename: recording.filename,
+    contentType: "audio/wav",
+  });
+
+  //create form-data headers function
+  function getHeaders(form: any) {
+    return new Promise((resolve, reject) => {
+      form.getLength((err: any, length: any) => {
+        if (err) {
+          reject(err);
+        }
+        let headers = Object.assign(
+          { "Content-Length": length },
+          form.getHeaders()
+        );
+        resolve(headers);
+      });
+    });
+  }
+
+  //function to identify music
+  const identifyMusic = async (API_KEY: string | undefined): Promise<any> => {
+    //define headers object
+    let headers: { [name: string]: any } = (await getHeaders(form)) as {};
+    //add more headers to headers objects
+    headers = {
+      ...headers,
+      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "shazam-core.p.rapidapi.com",
     };
 
-    //send request to shazam to identify the track
-    request(options, async (err, resp, body) => {
-      //delete the track from tmp directory
+    try {
+      //send the post request
+      let {
+        data,
+      } = await axios.post(
+        "https://shazam-core.p.rapidapi.com/v1/tracks/recognize",
+        form,
+        { headers }
+      );
+
+      //extract track from reponse
+      let { track } = data;
+
+      //delete file
       fs.unlink(recording.path, () => {});
 
-      if (err) next(err);
-
-      const { track } = await JSON.parse(body);
-
+      //if the track is not found send a response
       if (!track) {
         const song = {
           identified: false,
@@ -89,6 +115,7 @@ app.post("/shazam", upload.single("file"), (req, res, next) => {
         return res.json(song);
       }
 
+      //create track object
       const song = {
         identified: true,
         title: track.title,
@@ -96,25 +123,47 @@ app.post("/shazam", upload.single("file"), (req, res, next) => {
         coverart: track.images.coverart,
       };
 
+      //return the song
       return res.json(song);
-    });
-  } catch (error) {
-    next(error);
-  }
+    } catch (error) {
+      if (error.response) {
+        //CHECH IF ERROR STATUS CODE IS 405, THAT MEANS THAT THE MONTHLY QUOTA HAS BEEN REACHED
+        if (error.response.status === 405) {
+          //if keynumber is less than 3 (size of the keys array)
+          if (keyNumber < 3) {
+            //TRY WITH ANOTHER KEY
+            keyNumber++;
+            return identifyMusic(api_keys?.[keyNumber]);
+          }
+        }
+      }
+      //forward the error to the handler
+      next(error);
+    }
+  };
+
+  await identifyMusic(api_keys?.[keyNumber]);
 });
 
 //Error handler
-app.use((req, res, next) => {
-  const error = new Error("Not Found") as ResponseError;
-  error.status = 404;
-  next(error);
-});
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const error = new Error("Not Found") as ResponseError;
+    error.status = 404;
+    next(error);
+  }
+);
 
-app.use((error: ResponseError, req: express.Request, res: express.Response) => {
-  res.status(error.status || 500);
+app.use(((
+  error: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  res.status(error.status || error.response.status || 500);
   res.json({
     message: error.message,
   });
-});
+}) as express.ErrorRequestHandler);
 
-app.listen(3000, () => console.log("LISTENING ON 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("LISTENING ON 3000"));
